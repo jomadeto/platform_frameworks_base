1c729958cfcf7c208d3adea2dc0fd8f4424ff92c
jomadeto <54934119+jomadeto@users.noreply.github.com>
2019-09-21 21:56:23 +0200

    Add CPU info qs tile
    Source commit d44815141e562003806470bfa667707bef0dadc1

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -33,6 +33,7 @@ import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
+import com.android.systemui.qs.tiles.CPUInfoTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
 import com.android.systemui.qs.tiles.CompassTile;
@@ -128,6 +129,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new NfcTile(mHost);
             case "sound":
                 return new SoundTile(mHost);
+            case "cpuinfo":
+                return new CPUInfoTile(mHost);
             case "caffeine":
                 return new CaffeineTile(mHost);
             case "reboot":
